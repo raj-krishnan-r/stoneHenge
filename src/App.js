7510cb69db87f146f0837aef8c83048d575ad9d7
Raj Krishnan R <raj.krishnan@hotpack.com>
2020-12-17 15:55:41 +0400

    mre data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+    <div style={{textAlign:"center"}}>
+      <h1>SOFA</h1>
+      <p>System of Foreign Affairs, M.Square KIC</p>
+    
     </div>
+    <div style={{margin:"10em"}}>
+        <h2>
+        SOFA ?
+      </h2>
+      <p>
+      We are established for the support and aid of Citizens of KIC, living or travelling outside the Nation. Constituted by the Article 101 of Constitution of KIC by the 3rd Supreme Leader of Kalavoor. Since the last 200 years we are in the forefront of protecting the rights of KIC citizens. We are also the Chief Whistleblower to the Army of KIC on War-decisions. 
+      </p>
+    </div>
+    </React.Fragment>
   );
 }
 
